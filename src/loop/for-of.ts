7a8a7b67a6ea5a3b803b3fd6dce884fcef058c3d
jbeat <jbeat0630@gmail.com>
2024-12-11 15:52:52 +0900

    feat: 반복문 forin,forof,foreach
    - for...in 수정
    - for...of 수정
    - forEach 추가

--- src/loop/for-of.ts
+++ src/loop/for-of.ts
@@ -1,12 +1,14 @@
-import {NumberArray, UserObject} from "../mockData";
+import { NumberArray, UserObject } from "../mockData";
 
 /**
- * for...of 문
- * - 배열의 경우: 요소의 값을 순차적으로 순회함
+ * for...of 문은 보통 `배열` 순회할 때 사용됨
+ * - 배열의 값에 직접 접근하며, 인덱스가 필요 없을 때 유용
+ * - 배열 외에도 `String`, `Map`, `Set` 등 반복 가능한 객체에 사용 가능
+ * - `break`, `continue`를 사용할 수 있어 조건에 맞춰 반복을 제어할 수 있음
  *
- * for...of는 배열의 값을 직접 순회하기 때문에 인덱스를 사용할 필요가 없고,
- * 배열의 각 요소를 바로 사용할 수 있음
- * 객체를 순회할 때는 `for...in`을 사용하는 것이 더 직관적임
+ * 사용 시 주의점:
+ * - 배열이나 반복 가능한 객체를 순회할 때 사용
+ * - 객체를 순회하려면 `Object.entries()`나 `Object.keys()`와 같은 메서드를 사용하여 배열로 변환 후 순회 가능
  *
  * @example
  * for (const value of array) {
@@ -15,9 +17,10 @@ import {NumberArray, UserObject} from "../mockData";
  */
 
 for (const arrayValue of NumberArray) {
-  console.log(`Array value: ${arrayValue}`) // 배열의 값 출력
+  console.log(`Array value: ${arrayValue}`);
 }
-// 객체의 경우 for...of를 사용하려면 Object.entries()를 사용
+
+// 객체를 순회할 때는 Object.entries()를 사용하여 배열로 변환 후 for...of 사용
 for (const [key, value] of Object.entries(UserObject)) {
-  console.log(`Object key: ${key}, value: ${value}`); // 객체의 키와 값 출력
+  console.log(`Object key: ${key}, value: ${value}`);
 }
