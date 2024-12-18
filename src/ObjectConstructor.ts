5d34bf63b849fcca3813214cbd8732f40dbd1560
jbeat <jbeat0630@gmail.com>
2024-12-18 18:32:45 +0900

    feature: 객체 생성자와 클래스
    - 리팩토링 필요

---
+++ src/ObjectConstructor.ts
@@ -0,0 +1,76 @@
+// es6 Class
+class Calculator {
+  name?: string;
+
+  constructor(name?: string) {
+    this.name = name;
+  }
+
+  add(a: number, b: number) {
+    return a + b;
+  }
+
+  div(a: number, b: number) {
+    return a / b;
+  }
+
+  greet() {
+    return `Hello ${this.name}`;
+  }
+}
+
+const calc1 = new Calculator('kang');
+const calc2 = new Calculator();
+console.log(Calculator.prototype.add === calc1.add);
+console.log(calc1.add === calc2.add);
+console.log(calc1.div === calc2.div);
+console.log(calc1.greet());
+console.log(calc2.greet());
+console.log(calc1.add(2, 3)); // calc를 calc1로 수정
+console.log(calc1.div(6, 3)); // calc를 calc1로 수정
+
+
+// es5 Constructor Function
+// 인터페이스 선언
+interface CalculatorFnc {
+  name: string;
+  add(a: number, b: number): number;
+  div(a: number, b: number): number;
+  greet(): string;
+}
+
+// 생성자 함수 타입 선언 (ES5 스타일)
+interface CalculatorFncConstructor {
+  new(name?: string): CalculatorFnc;
+}
+
+// 생성자 함수 (ES5 스타일)
+function CalculatorFnc(this: CalculatorFnc, name: string) {
+  this.name = name;
+
+  this.add = function (a: number, b: number): number {
+    return a + b;
+  };
+
+  this.div = function (a: number, b: number): number {
+    return a / b;
+  };
+
+  this.greet = function (): string {
+    return `Hello ${this.name}`;
+  };
+}
+
+// 타입을 명시하여 인스턴스 생성
+
+const calcFnc = new (CalculatorFnc as any)('kang');
+const calcFnc2 = new (CalculatorFnc as any)('');
+
+console.log('====================');
+console.log(CalculatorFnc.prototype.add === calcFnc.add);
+console.log(calcFnc.add === calcFnc2.add);
+console.log(calcFnc.div === calcFnc2.div);
+console.log(calcFnc.greet());
+console.log(calcFnc2.greet());
+console.log(calcFnc.add(3, 3));
+console.log(calcFnc.div(6, 2));
